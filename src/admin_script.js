1e488af7c00affe36ae42ee97764d687161589cb
Konstantin Dubinets <1409357+kdubinets@users.noreply.github.com>
2024-02-24 18:37:36 +0000

    Improved formatting and validation of history JSON on the admin page

--- src/admin_script.js
+++ src/admin_script.js
@@ -3,6 +3,7 @@
 const historyTextArea = document.getElementById('history');
 const updateButton = document.getElementById('update');
 const results = document.getElementById('results');
+const error = document.getElementById('error');
 
 init();
 
@@ -13,9 +14,16 @@ function init() {
 }
 
 updateButton.addEventListener('click', function() {
-    storeHistory(historyTextArea.value);
-    results.style.display = 'block';
+    if (!storeHistory(historyTextArea.value)) {
+        error.style.display = 'block';
+        setTimeout(function() {
+            error.style.display = 'none';
+        }, 3000);    
 
+        return;
+    }
+
+    results.style.display = 'block';
     setTimeout(function() {
         results.style.display = 'none';
     }, 3000);    
@@ -23,10 +31,58 @@ updateButton.addEventListener('click', function() {
 
 
 function getHistory() {
-    return JSON.parse(localStorage.getItem('sessionsHistory')) || [];
+    const cached = localStorage.getItem('sessionsHistory');
+    if (!cached) {
+        return "";
+    }
+
+    const jsonObject = JSON.parse(cached);
+    const formattedJson = JSON.stringify(jsonObject, null,  2);
+    return formattedJson;
 }
 
 
 function storeHistory(history) {
-    localStorage.setItem('sessionsHistory', JSON.stringify(history));
+    try {
+        const jsonObject = JSON.parse(history);
+
+        if (!Array.isArray(jsonObject)) {
+            // report error
+            return false;
+        }
+
+        for (item of jsonObject) {
+            if (typeof item === 'object' && !Array.isArray(item)) {
+                // let's check that it has required field and they are of the correct types
+                if (!hasAtribute(item, 'startTime', 'string')) {
+                    return false;
+                }
+                if (!hasAtribute(item, 'plannedDurationSec', 'number')) {
+                    return false;
+                }
+                if (!hasAtribute(item, 'realDurationSec', 'number')) {
+                    return false;
+                }
+                if (!hasAtribute(item, 'succesfull', 'boolean')) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        localStorage.setItem('sessionsHistory', history);
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+
+function hasAtribute(obj, member, type) {
+    if (! obj.hasOwnProperty(member)) {
+        return false;
+    }
+    return typeof obj[member] === type;
 }
